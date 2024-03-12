1de77ca9523dff3ee42665ea8b63ce6436049927
suresh <8SURESHGHOSH@GMAIL.COM>
2024-03-12 16:57:11 +0530

    useContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import React from "react";
-import "./App.css";   
+  
 import Card from "./components/Card";
 import ProductTab from "./components/productTab";
 import Button from "./components/button";
 import Form from "./components/form"
 import LudoBoard from "./components/LudoBoard";
 import TodoList from "./components/TodoList";
+import PasswordGenerator from "./components/PasswordGenerator";
+import Component1 from "./components/useContext";
 
 function App(){
   return (
   <>
+  <PasswordGenerator/>
+  <Component1/>
  
 </>
   );
